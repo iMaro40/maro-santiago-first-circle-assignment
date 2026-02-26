c4d6193dab2d8e6da988e1cf882b6bc1df0175c0
iMaro40 <maro.santiago@yahoo.com>
2026-02-26 15:23:03 +0800

    initial repo setup. TS + ESLint + Prettier

---
+++ eslint.config.mjs
@@ -0,0 +1,22 @@
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+import globals from "globals";
+
+export default [
+  // Ignore build output
+  {
+    ignores: ["build/**"],
+  },
+
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+
+  {
+    files: ["**/*.ts"],
+    languageOptions: {
+      globals: {
+        ...globals.node, // gives console, process, etc
+      },
+    },
+  },
+];
